e30f5e574cfa005dce12416b0532a11395762227
Ankur <armanankur@gmail.com>
2022-04-10 11:46:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Sdata from './Sdata'
+import Card from './Card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+
+  
+    return (
+        <div>
+            <Card imgsrc={Sdata[0].imgsrc}
+sname={Sdata[0].sname}
+ stitle={Sdata[0].stitle}
+  link= {Sdata[0].link} /> 
+
+<Card imgsrc={Sdata[1].imgsrc}
+sname={Sdata[1].sname}
+ stitle={Sdata[1].stitle}
+  link= {Sdata[1].link} /> 
+
+  <Card imgsrc={Sdata[2].imgsrc}
+sname={Sdata[2].sname}
+ stitle={Sdata[2].stitle}
+  link= {Sdata[2].link} /> 
+
+  <Card imgsrc={Sdata[3].imgsrc}
+sname={Sdata[3].sname}
+ stitle={Sdata[3].stitle}
+  link= {Sdata[3].link} /> 
+
+<Card imgsrc={Sdata[4].imgsrc}
+sname={Sdata[4].sname}
+ stitle={Sdata[4].stitle}
+  link= {Sdata[4].link} /> 
+        </div>
+    )
 }
-
-export default App;
---
+++ src/Sdata.js
@@ -0,0 +1,33 @@
+ const Sdata = [
+    {
+ imgsrc:"https://wallpapercave.com/dwp1x/wp6291597.jpg",
+sname:'MONEY HEIST Season 1',
+ stitle:'A Netflix Originals',
+  link:"https://www.netflix.com/in/title/80192098" ,
+},
+    {
+imgsrc:"https://wallpapercave.com/dwp1x/wp4906374.jpg",
+sname:'MONEY HEIST Season 2' ,
+stitle:'A Netflix Originals',
+ link:"https://www.netflix.com/in/title/80192098" ,
+    },
+ {
+ imgsrc:"https://wallpapercave.com/dwp1x/wp5234773.jpg",
+sname:'MONEY HEIST Season 3',
+stitle:'A Netflix Originals' ,
+link:"https://www.netflix.com/in/title/80192098" ,
+    },
+  {
+ imgsrc:"https://wallpapercave.com/dwp1x/wp6201564.jpg",
+sname:'MONEY HEIST Season 4',
+ stitle:'A Netflix Originals' ,
+ link:"https://www.netflix.com/in/title/80192098" ,
+    },
+    {
+ imgsrc:"https://m.media-amazon.com/images/M/MV5BNDJkYzY3MzMtMGFhYi00MmQ4LWJkNTgtZGNiZWZmMTMxNzdlXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_FMjpg_UX1000_.jpg",
+ sname:'MONEY HEIST Season 5',
+ stitle:'A Netflix Originals' ,
+ link:"https://www.netflix.com/in/title/80192098" , 
+    }
+];
+export default Sdata;
